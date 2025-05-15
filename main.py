158cbeffb8377f28a7f71710e98ccb30fbe083c6
almafra <almafra@miamioh.edu>
2025-05-15 16:49:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def index():
                 body {
                     font-family: Arial, sans-serif;
                     margin: 40px;
-                    background-color: #8d8deb;
+                    background-color: #8deba7;
                 }
                 h2 {
                     color: #333;
